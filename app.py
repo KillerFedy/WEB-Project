f32574baa5bc252f34c488bf25008036c240c151
MSI\zuevf <zuev.f.s@gmail.com>
2022-03-15 07:00:47 +0300

    Add List of Products

---
+++ app.py
@@ -0,0 +1,29 @@
+from importlib.resources import path
+from flask import Flask, render_template
+from pathlib import Path
+
+
+app = Flask(__name__)
+
+POSTS_DIR = Path('products')
+
+if not POSTS_DIR.exists():
+    POSTS_DIR.mkdir()
+
+
+def get_products():
+    products = []
+    for file in POSTS_DIR.iterdir():
+        with open(file, 'rt', encoding="utf-8") as f:
+            lines = f.read().split('\n')
+        product = {"name": lines[0], "description": lines[1]}
+        products.append(product)
+    print(products)
+    return products
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title = "Мой блог", products = get_products())
