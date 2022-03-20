2415c33d89eb58474f0b4ed28648b5dc9406d98b
Rashit Khaliullin <rashit.xaliullin@inbox.ru>
2022-03-20 21:19:28 +1000

    Add images in categories

--- app.py
+++ app.py
@@ -15,18 +15,27 @@ if not POSTS_DIR.exists():
 if not CATEGORIES_DIR.exists():
     CATEGORIES_DIR.mkdir()
 
-def get_items(PATH):
-    items = []
-    for file in PATH.iterdir():
+def get_products():
+    products = []
+    for file in POSTS_DIR.iterdir():
         with open(file, 'rt', encoding="utf-8") as f:
             lines = f.read().split('\n')
-        item = {"name": lines[0], "description": lines[1]}
-        items.append(item)
-    return items
+        product = {"name": lines[0], "description": lines[1]}
+        products.append(product)
+    return products
+
+def get_category():
+    categories = []
+    for file in CATEGORIES_DIR.iterdir():
+        with open(file, 'rt', encoding="utf-8") as f:
+            lines = f.read().split('\n')
+        category = {"name": lines[0], "description": lines[1], 'img': lines[2]}
+        categories.append(category)
+    return categories
 
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = "Магазин Шаурмы", products = get_items(POSTS_DIR), categories = get_items(CATEGORIES_DIR))
+    return render_template('index.html', title = "Магазин Шаурмы", products = get_products(), categories = get_category())
