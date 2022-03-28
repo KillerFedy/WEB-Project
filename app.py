bdcfb01197a3b00c1a47ba583e4e0a6b408670a0
Rashit Khaliullin <rashit.xaliullin@inbox.ru>
2022-03-28 10:30:26 +1000

    some modify for show

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def get_category():
 
 @app.route('/')
 @app.route('/index')
-#def index():
-#    return render_template('index.html', title = "Магазин Шаурмы", products = get_products(), categories = get_category())
+def index():
+    return render_template('index.html', title = "Магазин Шаурмы", products = get_products(), categories = get_category())
 
-def product():
-    return render_template('product.html', title = "Шаурма куриная")
+#def product():
+#    return render_template('product.html', title = "Шаурма куриная")
